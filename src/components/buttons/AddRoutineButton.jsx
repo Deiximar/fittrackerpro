48129e81e4e7712a027a05f251b229bf240c55ef
Deiximar Virriel <deiximar@gmail.com>
2024-09-12 13:53:15 +0200

    Create add routine button component

---
+++ src/components/buttons/AddRoutineButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PlusIcon from "../icons/PlusIcon";
+
+function AddRoutineButton() {
+  return (
+    <button className="add-routine">
+      <PlusIcon />
+      Routine
+    </button>
+  );
+}
+
+export default AddRoutineButton;
