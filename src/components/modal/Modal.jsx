f22b2a7672a7d1aa2daaa41dfaa7ae9178662784
Deiximar Virriel <deiximar@gmail.com>
2024-09-13 09:34:07 +0200

    Create modal alert

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,13 @@
+import "./modal.scss"; // Importa los estilos del modal
+
+const Modal = ({ open, children }) => {
+  if (!open) return null;
+
+  return (
+    <div className="modal isopen">
+      <section className="modal-container">{children}</section>
+    </div>
+  );
+};
+
+export default Modal;
