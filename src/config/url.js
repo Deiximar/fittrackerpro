148fd7103c74929c8f959a389ca0e09fbdf155a5
Deiximar Virriel <deiximar@gmail.com>
2024-09-13 02:34:51 +0200

    Create user api

--- src/config/url.js
+++ src/config/url.js
@@ -9,3 +9,9 @@ export const GET_EXERCISES = BASE_URL + "exercises";
 export const GET_ROUTINES = BASE_URL + "routines";
 
 export const GET_USER_ROUTINES = GET_ROUTINES + "/my-routines"
+
+
+// USER URL
+export const USER_REGISTER = BASE_URL + "auth/register";
+
+export const USER_LOGIN = BASE_URL + "auth/login";
