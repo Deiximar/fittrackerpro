18db6892e219aef63f16f45f75685e0caa2b9aa4
Deiximar Virriel <deiximar@gmail.com>
2024-10-16 16:48:27 +0200

    Limit routine widgets

--- src/service/routineApi.jsx
+++ src/service/routineApi.jsx
@@ -1,28 +1,29 @@
 import axios from "axios";
 import { GET_ROUTINES, GET_USER_ROUTINES } from "../config/url";
 
-export const fetchRoutines = async () => {
-  let headers = undefined;
-  const token = null;
-
-  if (token) {
-    headers = { Authorization: `Bearer ${token}` };
-  }
+export const fetchRoutines = async (pageParam = 0) => {
   const res = await axios.get(GET_ROUTINES, {
-    headers,
+    params: {
+      page: pageParam,
+      size: 4,
+    },
   });
+  console.log(res);
   return res.data;
 };
 
-export const fetchUserRoutines = async () => {
-  let headers = undefined;
-  const token = null;
-
+export const fetchUserRoutines = async ({ queryKey }) => {
+  const [_key, token, pageParam] = queryKey;
+  let headers = {};
   if (token) {
-    headers = { Authorization: `Bearer ${token}` };
+    headers.Authorization = `Bearer ${token}`;
   }
   const res = await axios.get(GET_USER_ROUTINES, {
     headers,
+    params: {
+      page: pageParam,
+      size: 4,
+    },
   });
   return res.data;
 };
