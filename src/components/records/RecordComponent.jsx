ccdea4f97f4166793d5eef7c8186f3be442085d8
Deiximar Virriel <deiximar@gmail.com>
2025-02-03 10:24:00 +0100

    Create and style record component

--- src/components/records/RecordComponent.jsx
+++ src/components/records/RecordComponent.jsx
@@ -4,6 +4,34 @@ function RecordComponent() {
   return (
     <div className="record-container">
       <h2 className="title">History</h2>
+
+      <div className="month-container">
+        <h3 className="month-title">August 2024</h3>
+        <div className="record-day-container">
+          <h4 className="subtitle">Routine</h4>
+          <h5 className="day-subtitle">Tuesday, 27 Aug</h5>
+          <div className="exercise-container">
+            <div>
+              <h4 className="subtitle">Exercise</h4>
+              <ul className="exercise-name-list">
+                <li>3 x exercise name</li>
+                <li>3 x exercise name</li>
+                <li>3 x exercise name</li>
+                <li>3 x exercise name</li>
+              </ul>
+            </div>
+            <div>
+              <h4 className="subtitle">Best Set</h4>
+              <ul className="best-set-list">
+                <li>20 kg x 15</li>
+                <li>20 kg x 15</li>
+                <li>20 kg x 15</li>
+                <li>20 kg x 15</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
